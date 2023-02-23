84684a3507fd9a02e9a82a078117512b0ae39e2c
Sushant <sushantwalunj12@gmail.com>
2023-02-24 00:13:37 +0530

    Sushant

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const evaluatedText = document.getElementById('evaluatedText');
+const wordCount = document.getElementById('wordCount');
+
+evaluatedText.addEventListener('input', () => {
+  const words = evaluatedText.value.match(/\b[-?(\w+)?]+\b/gi);
+  wordCount.textContent = words ? words.length : 0;
+});
